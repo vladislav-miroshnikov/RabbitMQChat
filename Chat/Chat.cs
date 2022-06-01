b009c120c11aeeb97521e8e2222fb939fcc0acd1
Kuznetsov Ilya Alexandrovich <kuznetsov.ilya.alexandrovich@gmail.com>
2022-06-01 14:34:41 +0300

    Added queue declaring and command loop

--- Chat/Chat.cs
+++ Chat/Chat.cs
@@ -24,6 +24,9 @@ public class Chat
         clientGuid = Guid.NewGuid();
 
         CreateConnection(host);
+        DeclareQueue(queue);
+        
+        Start();
     }
 
     private void CreateConnection(string hostName)
@@ -50,5 +53,74 @@ public class Chat
             };
         });
         asyncHandlerThread.Start();
+    }
+    
+    private void DeclareQueue(string baseQueue)
+    {
+        this.baseQueue = baseQueue;
+        _currentQueue = baseQueue + "$" + clientGuid;
+        
+        channel.QueueDeclare(queue: _currentQueue,
+            durable: false,
+            exclusive: true,
+            autoDelete: false,
+            arguments: null);
+        
+        channel.BasicConsume(queue: _currentQueue,
+            autoAck: true,
+            consumer: consumer);
+        channel.QueueBind(_currentQueue, "topic_chat", baseQueue + "$*", null);
+    }
+
+    private void Start()
+    {
+        ChatUtils.PrintChatInfo();
+        while (true)
+        {
+            var input = Console.ReadLine();
+            ChatUtils.ClearCurrentConsoleLine();
+            string message, command = string.Empty;
+            
+            if (input.StartsWith('#'))
+            {
+                var splittedInput = input.Split(' ');
+                
+                command = splittedInput[0];
+                message = splittedInput.Length > 1 ? splittedInput[1] : string.Empty;
+            }
+            else
+            {
+                message = input;
+            }
+
+            switch (command)
+            {
+                case "":
+                    var body = Encoding.UTF8.GetBytes(message);
+                    var properties = channel.CreateBasicProperties();
+                    
+                    properties.MessageId = clientName + ' ' + clientGuid;
+                    channel.BasicPublish(exchange: "topic_chat",
+                        routingKey: baseQueue + "$*",
+                        basicProperties: properties,                  
+                        body: body);
+                    
+                    Console.WriteLine($"{clientName} >> {message}");
+                    break;
+                case "#switch":
+                    DeclareQueue(message);
+                    
+                    Console.Clear();
+                    Console.WriteLine($"{clientName} switched to {message}");
+                    break;
+                case "#exit":
+                    asyncHandlerThread.Dispose();
+                    channel.Close();
+                    connection.Close();
+                    
+                    Console.WriteLine($"{clientName} disconnected");
+                    return;
+            }
+        }
     }
 }
--- Chat/ChatUtils.cs
+++ Chat/ChatUtils.cs
@@ -2,6 +2,15 @@
 
 public static class ChatUtils
 {
+    public static void ClearCurrentConsoleLine()
+    {
+        Console.SetCursorPosition(0, Console.CursorTop - 1);
+        var currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth)); 
+        Console.SetCursorPosition(0, currentLineCursor);
+    }
+
     public static void PrintChatInfo()
     {
         Console.WriteLine("Welcome to Chat!\nYou can use following command:" +
